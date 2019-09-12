c5b10dd3217bbb29c7f5fe433fdf1f147fad3e43
laixintao <laixintao1995@163.com>
2019-09-12 19:42:44 +0800

    migrations

--- example/app1/models.py
+++ example/app1/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=64)
     age = models.IntegerField()
+    intc = models.IntegerField()
+    intd = models.IntegerField()
