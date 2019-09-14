85ad1c036b1259bc002c587300609e6f5118bbdf
laixintao <laixintao1995@163.com>
2019-09-14 17:27:30 +0800

    alter

---
+++ south_example/app1/migrations/0002_auto__add_field_knight_dances_whenever_able.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Knight.dances_whenever_able'
+        db.add_column('app1_knight', 'dances_whenever_able',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Knight.dances_whenever_able'
+        db.delete_column('app1_knight', 'dances_whenever_able')
+
+
+    models = {
+        'app1.knight': {
+            'Meta': {'object_name': 'Knight'},
+            'dances_whenever_able': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'of_the_round_table': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        }
+    }
+
+    complete_apps = ['app1']
--- south_example/app1/models.py
+++ south_example/app1/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Knight(models.Model):
     name = models.CharField(max_length=100)
     of_the_round_table = models.BooleanField()
+    dances_whenever_able = models.BooleanField()
